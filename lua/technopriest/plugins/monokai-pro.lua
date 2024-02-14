b9dbf02d65c82773c2a31c3374bb8be258c32b36
supremetechnopriest <randylebeau@gmail.com>
2024-02-14 14:59:35 -0800

    🐛 Fix split navigation.

--- lua/technopriest/plugins/monokai-pro.lua
+++ lua/technopriest/plugins/monokai-pro.lua
@@ -1,10 +1,10 @@
 return {
-  'loctvl842/monokai-pro.nvim',
-  config = function()
-    require('monokai-pro').setup({
-      filter = 'ristretto'
-    })
+	"loctvl842/monokai-pro.nvim",
+	config = function()
+		require("monokai-pro").setup({
+			filter = "ristretto",
+		})
 
-    vim.cmd.colorscheme = 'monokai-pro'
-  end
+		vim.cmd.colorscheme = "monokai-pro"
+	end,
 }
